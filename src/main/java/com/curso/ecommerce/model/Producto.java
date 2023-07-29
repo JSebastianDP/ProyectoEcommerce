a38d4e75494882c1c089c50115daa39bc2a22152
JSebastianDP <sebastiandiaz28001@gmail.com>
2023-07-28 23:41:13 -0500

    Se realiza la conexion a la base de datos, queda pendiente volver a revisar el mapeado de los datos

--- src/main/java/com/curso/ecommerce/model/Producto.java
+++ src/main/java/com/curso/ecommerce/model/Producto.java
@@ -1,6 +1,17 @@
 package com.curso.ecommerce.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity 
+@Table(name = "productos")
 public class Producto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer  id;
 	private String nombre;
 	private String descripcion;
@@ -8,13 +19,17 @@ public class Producto {
 	private Double precio;
 	private int cantidad;
 	
+	@ManyToOne
+	private Usuario usuario;
+	
 	
 public Producto() {
 	// TODO Auto-generated constructor stub
 }
 
 
-public Producto(Integer id, String nombre, String descripcion, String imagen, Double precio, int cantidad) {
+public Producto(Integer id, String nombre, String descripcion, String imagen, Double precio, int cantidad,
+		Usuario usuario) {
 	super();
 	this.id = id;
 	this.nombre = nombre;
@@ -22,7 +37,9 @@ public Producto(Integer id, String nombre, String descripcion, String imagen, Do
 	this.imagen = imagen;
 	this.precio = precio;
 	this.cantidad = cantidad;
+	this.usuario = usuario;
 }
+
 
 
 public Integer getId() {
@@ -60,6 +77,16 @@ public int getCantidad() {
 }
 public void setCantidad(int cantidad) {
 	this.cantidad = cantidad;
+}
+
+
+public Usuario getUsuario() {
+	return usuario;
+}
+
+
+public void setUsuario(Usuario usuario) {
+	this.usuario = usuario;
 }
 
 
