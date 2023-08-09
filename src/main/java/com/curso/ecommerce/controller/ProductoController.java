a8c49e921a4cba66f883cf320a0f0f090e23bea8
JSebastianDP <sebastiandiaz28001@gmail.com>
2023-08-08 23:28:12 -0500

    Se termina la funcionalidad de la imagen en los productos, ya se agrega, se elimina y se actualiza

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -1,5 +1,6 @@
 package com.curso.ecommerce.controller;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.Optional;
 
@@ -19,71 +20,93 @@ import com.curso.ecommerce.model.Usuario;
 import com.curso.ecommerce.service.ProductoService;
 import com.curso.ecommerce.service.UploadFileService;
 
-
 @Controller
 @RequestMapping("/productos")
 public class ProductoController {
-	
-	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class); 
-	
+
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductoController.class);
+
 	@Autowired
 	private ProductoService productoService;
-	
+
 	@Autowired
 	private UploadFileService upload;
-	
+
 	@GetMapping("")
-	public String show(Model model){
+	public String show(Model model) {
 		model.addAttribute("productos", productoService.findAll());
 		return "productos/show";
 	}
-	
+
 	@GetMapping("/create")
 	public String create() {
 		return "productos/create";
 	}
+
 	@PostMapping("/save")
-	public String save(Producto producto, @RequestParam("img")MultipartFile file ) throws IOException {
+	public String save(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
 		LOGGER.info("Este es el objeto producto {}", producto);
 		Usuario u = new Usuario(1, "", "", "", "", "", "", "");
 		producto.setUsuario(u);
-		
-		//logica para imagen
-		if(producto.getId() == null) {
+
+		// logica para imagen
+		if (producto.getId() == null) {
 			String nombreImagen = upload.saveImage(file);
 			producto.setImagen(nombreImagen);
-		}else {
-			if(file.isEmpty()) {
-				Producto p = new Producto();
-				p =productoService.get(producto.getId()).get();
-				producto.setImagen(p.getImagen());
-			}else {
-				String nombreImagen = upload.saveImage(file);
-				producto.setImagen(nombreImagen);
-			}
+		} else {
+
 		}
-		
+
 		productoService.save(producto);
 		return "redirect:/productos";
 	}
+
 	@GetMapping("/edit/{id}")
 	public String edit(@PathVariable Integer id, Model model) {
-		Producto producto = new Producto(); 
+		Producto producto = new Producto();
 		Optional<Producto> optionalProducto = productoService.get(id);
 		producto = optionalProducto.get();
 		LOGGER.info("Producto buscado: {}", producto);
 		model.addAttribute("producto", producto);
 		return "productos/edit";
 	}
+
 	@PostMapping("/update")
-	public String update(Producto producto) {
+	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+		if (file.isEmpty()) {
+			Producto p = new Producto();
+			p = productoService.get(producto.getId()).get();
+			producto.setImagen(p.getImagen());
+		} else {
+			Producto p = new Producto();
+			p = productoService.get(producto.getId()).get();
+			// borra la imagen siempre y cuando no sea la imagen que está por defecto
+			if (!p.getImagen().equals("default.jpg")) {
+				upload.deleteImage(p.getImagen());
+
+			}
+			
+			String nombreImagen = upload.saveImage(file);
+			producto.setImagen(nombreImagen);
+		}
 		productoService.Update(producto);
 		return "redirect:/productos";
+
 	}
+
 	@GetMapping("/delete/{id}")
 	public String delete(@PathVariable Integer id) {
+
+		Producto p = new Producto();
+		p = productoService.get(id).get();
+		// borra la imagen siempre y cuando no sea la imagen que está por defecto
+		if (!p.getImagen().equals("default.jpg")) {
+			upload.deleteImage(p.getNombre());
+
+		}
+
 		productoService.Delete(id);
-		return "redirect:/productos"; 
+		return "redirect:/productos";
 	}
-	
+
 }
