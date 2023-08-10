bcf8e6ecb34048239b48da2c56367f9285114cc0
JSebastianDP <sebastiandiaz28001@gmail.com>
2023-08-09 23:21:21 -0500

    Se realiza la funcionalidad del listado de productos en el home, adicionalmente se borra redundancia de codigo

--- src/main/java/com/curso/ecommerce/controller/ProductoController.java
+++ src/main/java/com/curso/ecommerce/controller/ProductoController.java
@@ -73,13 +73,12 @@ public class ProductoController {
 
 	@PostMapping("/update")
 	public String update(Producto producto, @RequestParam("img") MultipartFile file) throws IOException {
+		Producto p = new Producto();
+		p = productoService.get(producto.getId()).get();
+		
 		if (file.isEmpty()) {
-			Producto p = new Producto();
-			p = productoService.get(producto.getId()).get();
 			producto.setImagen(p.getImagen());
 		} else {
-			Producto p = new Producto();
-			p = productoService.get(producto.getId()).get();
 			// borra la imagen siempre y cuando no sea la imagen que está por defecto
 			if (!p.getImagen().equals("default.jpg")) {
 				upload.deleteImage(p.getImagen());
@@ -89,6 +88,7 @@ public class ProductoController {
 			String nombreImagen = upload.saveImage(file);
 			producto.setImagen(nombreImagen);
 		}
+		producto.setUsuario(p.getUsuario());
 		productoService.Update(producto);
 		return "redirect:/productos";
 
