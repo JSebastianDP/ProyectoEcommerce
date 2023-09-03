b5d10f527626f9452aedb0ff7c465061789e3de2
JSebastianDP <sebastiandiaz28001@gmail.com>
2023-09-03 18:24:36 -0500

    Se implementa la funcionalidad de ver detalle de compra

--- src/main/java/com/curso/ecommerce/service/iOrdenService.java
+++ src/main/java/com/curso/ecommerce/service/iOrdenService.java
@@ -1,6 +1,7 @@
 package com.curso.ecommerce.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import com.curso.ecommerce.model.Orden;
 import com.curso.ecommerce.model.Usuario;
@@ -11,5 +12,6 @@ public interface iOrdenService {
 	Orden save(Orden orden);
 	String generarNumeroOrden();
 	List<Orden> findByUsuario (Usuario usuario);
+	Optional<Orden> findById(Integer id);
 
 }
